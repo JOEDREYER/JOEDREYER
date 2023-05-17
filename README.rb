329f302210ebc02ebbaa0cd79e22554abd792edb
JoeDreyer <111522837+JOEDREYER@users.noreply.github.com>
2023-05-17 17:16:48 +0200

    Update README.rb

--- README.rb
+++ README.rb
@@ -1,29 +1,40 @@
 class Joe < Human
   def initialize
-   @name = 'Joe Dreyer'
-   @age = 32
-   @nationality = ['South African', 'Irish', 'Irish Passport']
-   @education = [ 'Stellenbosch University', 'Le Wagon', 'IH Sydney' ]
-   @hobbies = [ 'Surfing', 'Boardgames', 'Hiking' ]
- end
-
-  def current_location
-    'Cape Town, South Africa'
+    @name = 'Joe Dreyer'
+    @age = 32
+    @nationality = ['South African', 'Irish']
+    @education = [ 'Stellenbosch University', 'Le Wagon', 'IH Sydney' ]
+    @hobbies = [ 'Surfing', 'Boardgames', 'Hiking' ]
+    @location = 'Cape Town, South Africa'
+    @open_for_work = true
+    @current_activity = {
+      coding: ['A boardgame meetup app: Game Plan'],
+      reading: [ 'The pragmatic programmer', 'The mastery of love'],
+      listening_to: ['Sam Fender']
+    }
   end
-  
-   def relocate!
-    'Anywhere in Europe'
+
+  def introduce
+    puts "Hey there, I'm #{@name}!"
+    puts "I'm a #{@nationality[0]} and #{@nationality[1]} with an #{@nationality[2]}."
+    puts "I'm #{@age} years old and I'm currently located in #{@location}."
+    puts "I graduated from #{@education[0]}, learned coding at #{@education[1]}, and also studied at #{@education[2]}."
+    puts "When I'm not coding, you can find me #{@hobbies[0]}, playing #{@hobbies[1]}, or #{@hobbies[2]}."
+  end
+
+  def relocate!
+    @location = 'Anywhere in Europe'
+    puts "Great news! I've relocated to #{@location}."
   end
 
   def open_for_work?
-   true
+    @open_for_work ? "Yes, I'm open for work!" : "I'm not currently looking for work."
   end
 
   def currently
-   {
-     coding: ['A boardgame meetup app: Game Plan'],
-     reading: [ 'The pragmatic programmer', 'The mastery of love'],
-     listening_to: ['Sam Fender']
-   }
+    puts "Right now, I'm busy with a few things:"
+    @current_activity.each do |activity, details|
+      puts "I'm #{activity.to_s.gsub('_', ' ')}: #{details.join(', ')}."
+    end
   end
 end
